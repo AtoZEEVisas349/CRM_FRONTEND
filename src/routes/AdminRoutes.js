46240d3ee055a4e5b695f1c277a302f5764abb4f
sagar <sagar90160@gmail.com>
2025-05-03 15:13:45 +0530

    Updated code

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -5,7 +5,6 @@ import AssignTask from "../features/admin/AssignTask";
 import ExecutiveDetails from "../features/admin/ExecutiveDetails";
 import AdminSettings from "../features/admin-settings/AdminSettings";
 import ContactUs from "../features/admin/ContactUs";
-import Cast from "../features/admin/Cast";
 
 const AdminRoutes = () => {
   return (
